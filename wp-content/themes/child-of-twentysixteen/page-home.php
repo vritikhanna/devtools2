40b22569098a81a06eeefb263d5e177afa1174f0
vriti <vriti@Gagandeeps-MacBook-Pro.local>
2018-08-09 23:16:34 -0400

    created custom templates no sidebar

--- wp-content/themes/child-of-twentysixteen/page-home.php
+++ wp-content/themes/child-of-twentysixteen/page-home.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying the home page.
+ * The template for displaying the home page
  *
  * This is the template that displays all pages by default.
  * Please note that this is the WordPress construct of pages and that
